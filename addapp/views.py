7672d74b628ab397afda0b47bbcfda13058ffafc
vennela29 <vennelashekar2000@gmail.com>
2022-07-14 08:56:21 +0530

    Initial commit

---
+++ addapp/views.py
@@ -0,0 +1,6 @@
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def welcome(request):
+    return render(request, 'home.html')
